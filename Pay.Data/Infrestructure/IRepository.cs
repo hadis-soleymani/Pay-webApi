d776c51c076017703d9b8bf47140ff5ccb33e23a
hadis soleymani <hadissoleymani.it@gmail.com>
2023-08-18 20:00:20 +0330

    add common repository

---
+++ Pay.Data/Infrestructure/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+
+namespace Pay.Data.Infrestructure
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        #region Insert
+        void Insert(TEntity entity);
+        Task InsertAsync(TEntity entity);
+        #endregion
+        #region Update
+        void Update(TEntity entity);
+        #endregion
+        #region Delete
+        void Delete(object id);
+        void Delete(TEntity entity);
+        void Delete(Expression<Func<TEntity, bool>> where);
+        #endregion
+        #region Get
+        TEntity GetById(object id);
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(Expression<Func<TEntity, bool>> where);
+        IEnumerable<TEntity> GetMany(Expression<Func<TEntity, bool>> where);
+
+        Task<TEntity> GetByIdAsync(object id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> where);
+        Task<IEnumerable<TEntity>> GetManyAsync(Expression<Func<TEntity, bool>> where);
+        #endregion
+    }
+}
