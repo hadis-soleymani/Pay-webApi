9e894c8323f333fb51a6f1f5dbd90a0f273fece8
hadis soleymani <hadissoleymani.it@gmail.com>
2023-08-18 12:33:06 +0330

    add unit of work

---
+++ Pay.Data/Infrestructure/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pay.Data.Infrestructure
+{
+    public interface IUnitOfWork<TContext> : IDisposable where TContext : DbContext
+    {
+        void Save();
+        Task<int> SaveAsync();
+    }
+}
---
+++ Pay.Data/Infrestructure/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pay.Data.Infrestructure
+{
+    public class UnitOfWork<TContext> : IUnitOfWork<TContext> where TContext : DbContext, new()
+    {
+        #region ctor
+        protected readonly DbContext _db;
+        public UnitOfWork()
+        {
+            _db = new TContext();
+        }
+        #endregion
+        #region save
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public Task<int> SaveAsync()
+        {
+            return _db.SaveChangesAsync();
+        }
+        #endregion
+        #region dispose
+        private bool dispose = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (!dispose)
+                {
+                    _db.Dispose();
+                }
+            }
+            dispose = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        ~UnitOfWork()
+        {
+            Dispose(false);
+        }
+        #endregion
+    }
+}
